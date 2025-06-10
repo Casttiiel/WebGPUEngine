8562f562fa454bf6051ab1d9cd5d02c6a73b5b66
Dani <casttiiel@gmail.com>
2025-06-10 17:47:51 +0200

    No erros on init

--- assets/shaders/gbuffer.fs
+++ assets/shaders/gbuffer.fs
@@ -1,6 +1,14 @@
 @group(2) @binding(0) var textureSampler: texture_2d<f32>;
 @group(2) @binding(1) var samplerState: sampler;
 
+struct VertexOutput {
+    @builtin(position) position: vec4<f32>,
+    @location(0) N: vec3<f32>,
+    @location(1) Uv: vec2<f32>,
+    @location(2) WorldPos: vec3<f32>,
+    @location(3) T: vec4<f32>,
+}
+
 struct FragmentOutput {
     @location(0) albedo: vec4<f32>,
     @location(1) normal: vec4<f32>,
