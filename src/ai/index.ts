87777d0ee6f2e952793037b9d1d6e384b77042b2
Dani <casttiiel@gmail.com>
2026-03-06 10:46:52 +0100

    .

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,15 +1,15 @@
 // Core
-export { BehaviorNode, Status }    from './BehaviorNode';
-export { Blackboard }              from './Blackboard';
-export { BehaviorTree }            from './BehaviorTree';
+export { BehaviorNode, Status } from './BehaviorNode';
+export { Blackboard } from './Blackboard';
+export { BehaviorTree } from './BehaviorTree';
 
 // Composite nodes
-export { Sequence }                from './nodes/Sequence';
-export { Selector }                from './nodes/Selector';
+export { Sequence } from './nodes/Sequence';
+export { Selector } from './nodes/Selector';
 
 // Leaf nodes
-export { Action }                  from './nodes/Action';
-export { Condition }               from './nodes/Condition';
+export { Action } from './nodes/Action';
+export { Condition } from './nodes/Condition';
 
 // Decorators
 export { Inverter, AlwaysSucceed, Repeat } from './nodes/Decorators';
